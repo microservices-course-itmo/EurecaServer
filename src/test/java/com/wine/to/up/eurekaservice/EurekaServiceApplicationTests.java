2eec9710200403ef0a34fd9db77bbef6b67afd47
Roman Neviantsev <>
2021-02-24 19:55:46 +0500

    Intial commit

---
+++ src/test/java/com/wine/to/up/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wine.to.up.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
